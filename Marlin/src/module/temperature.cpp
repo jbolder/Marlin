0d9c422c2e7737ca11031f0945c6498bcf10bf44
jbolder <97024935+jbolder@users.noreply.github.com>
2022-10-04 20:26:39 +0200

    do not disable steppers on emergency stop to avoid z crash

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -1294,7 +1294,7 @@ void Temperature::manage_heater() {
   REMEMBER(mh, no_reentry, true);
 
   #if ENABLED(EMERGENCY_PARSER)
-    if (emergency_parser.killed_by_M112) kill(M112_KILL_STR, nullptr, true);
+    if (emergency_parser.killed_by_M112) kill(M112_KILL_STR, nullptr, false);
 
     if (emergency_parser.quickstop_by_M410) {
       emergency_parser.quickstop_by_M410 = false; // quickstop_stepper may call idle so clear this now!
