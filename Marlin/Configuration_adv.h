91af6a4539527877144aeae2fd27569401c7d97a
jbolder <97024935+jbolder@users.noreply.github.com>
2022-10-08 21:12:13 +0200

    changed serial port config, increased current to 1500mA, added dummy code for kill pin

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2590,7 +2590,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT 1200 // 800        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT 1500 // 800        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11
@@ -2599,7 +2599,7 @@
   #endif
 
   #if AXIS_IS_TMC(X2)
-    #define X2_CURRENT 900 // 800
+    #define X2_CURRENT 1500 // 800
     #define X2_CURRENT_HOME X2_CURRENT
     #define X2_MICROSTEPS    X_MICROSTEPS
     #define X2_RSENSE         0.11
@@ -2608,7 +2608,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT 1200 // 800
+    #define Y_CURRENT 1500 // 800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
@@ -2617,7 +2617,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y2)
-    #define Y2_CURRENT 1200 // 800
+    #define Y2_CURRENT 1500 // 800
     #define Y2_CURRENT_HOME Y2_CURRENT
     #define Y2_MICROSTEPS    Y_MICROSTEPS
     #define Y2_RSENSE         0.11
@@ -2626,7 +2626,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT 1200 // 800
+    #define Z_CURRENT 900 // 800
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2635,7 +2635,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z2)
-    #define Z2_CURRENT 1200 // 800
+    #define Z2_CURRENT 900 // 800
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS    Z_MICROSTEPS
     #define Z2_RSENSE         0.11
