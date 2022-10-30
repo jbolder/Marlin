a5107e10a33611d279dd176e58dc373198e9aa7f
jbolder <97024935+jbolder@users.noreply.github.com>
2022-10-30 19:11:44 +0100

    disable inactive stepper timeout

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -979,13 +979,13 @@
  * The Deactive Time can be overridden with M18 and M84. Set to 0 for No Timeout.
  */
 #define DEFAULT_STEPPER_DEACTIVE_TIME 1200 // 120
-#define DISABLE_INACTIVE_X true
-#define DISABLE_INACTIVE_Y true
+#define DISABLE_INACTIVE_X false
+#define DISABLE_INACTIVE_Y false
 #define DISABLE_INACTIVE_Z false // true  // Set 'false' if the nozzle could fall onto your printed part!
-#define DISABLE_INACTIVE_I true
-#define DISABLE_INACTIVE_J true
-#define DISABLE_INACTIVE_K true
-#define DISABLE_INACTIVE_E true
+#define DISABLE_INACTIVE_I false
+#define DISABLE_INACTIVE_J false
+#define DISABLE_INACTIVE_K false
+#define DISABLE_INACTIVE_E false
 
 // Default Minimum Feedrates for printing and travel moves
 #define DEFAULT_MINIMUMFEEDRATE       0.0     // (mm/s) Minimum feedrate. Set with M205 S.
