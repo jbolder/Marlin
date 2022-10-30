8e3b7613511e610560b8f428eeaa6dc76871bd6d
jbolder <97024935+jbolder@users.noreply.github.com>
2022-10-30 19:15:21 +0100

    updated X Y Z feedrates to 75 75 (50% higher) 10 (33% lower)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -938,7 +938,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE { 50, 50, 15 } // { 50, 50, 15, 25 } // { 300, 300, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE { 75, 75, 10 } // { 50, 50, 15, 25 } // { 300, 300, 5, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
