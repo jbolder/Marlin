ef6af833f466ad71db34645dea954d9ab02af4c0
jbolder <97024935+jbolder@users.noreply.github.com>
2023-01-04 13:31:40 +0100

    update work area, to fix z and x collision issues

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1373,7 +1373,7 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 629 // 200
+#define X_BED_SIZE 627 // 629 // 200
 #define Y_BED_SIZE 1390 // 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
@@ -1382,7 +1382,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 79.5
+#define Z_MAX_POS 77.5 // 79.5
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
